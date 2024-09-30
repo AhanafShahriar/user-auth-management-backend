84158b73f09ed2034887fe50077eb5ab4e607ce3
AhanafShahriar <t.m.ahanafshahriar@gmail.com>
2024-09-30 09:27:12 +0600

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://user-auth-management-frontend.onrender.com",
+  })
+);
 
 app.use(express.json());
 
