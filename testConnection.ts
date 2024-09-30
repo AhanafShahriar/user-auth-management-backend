1d75b3a7f233c0b569cc1d7efaf93e6e3deca6b7
AhanafShahriar <t.m.ahanafshahriar@gmail.com>
2024-09-30 09:17:25 +0600

    internal database link updated

--- testConnection.ts
+++ testConnection.ts
@@ -12,15 +12,16 @@ const pool = new Pool({
 
 const createTableQuery = `
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    last_login TIMESTAMP DEFAULT NULL,
-    status VARCHAR(10) CHECK (status IN ('Active', 'Blocked')) DEFAULT 'active',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    blocked BOOLEAN DEFAULT FALSE
-);
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(100) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL,
+      last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      status VARCHAR(20) DEFAULT 'active' CHECK (status IN ('active', 'blocked')),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      blocked BOOLEAN DEFAULT FALSE
+    );
+
 `;
 
 (async () => {
@@ -28,7 +29,6 @@ CREATE TABLE IF NOT EXISTS users (
     const client = await pool.connect();
     console.log("Connected to PostgreSQL database");
 
-    // Execute the create table query
     await client.query(createTableQuery);
     console.log("Users table created successfully");
 
